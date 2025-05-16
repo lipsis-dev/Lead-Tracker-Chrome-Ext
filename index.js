0a4b6f81b0d380ac8e9d85835dcb265036e9c694
Dadrea <rm4982@gmail.com>
2025-05-15 20:01:28 -0400

    Created a reactive web app, that connects to a real time Firebase Database.

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-let myLeads = []
+//Firebase Connections
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-database.js"
+
+const firebaseConfig = {
+    databaseURL: "https://leadstrackerapp-b27a9-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
+const referenceInDB = ref(database, "leads")
+
+//Application Functional Area
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
-const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
-const tabBtn = document.getElementById("tab-btn")
 
-if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage
-    render(myLeads)
-}
-
-tabBtn.addEventListener("click", function(){    
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-        render(myLeads)
-    })
-})
 
 function render(leads) {
     let listItems = ""
@@ -33,15 +31,31 @@ function render(leads) {
     ulEl.innerHTML = listItems
 }
 
+onValue(referenceInDB, function(snapshot) {
+    const snapshotDoesExist = snapshot.exists()
+    if (snapshotDoesExist){
+        const snapshotValues = snapshot.val()
+        // const leads = []
+
+        const leads = Object.values(snapshotValues)
+
+        //Alternate way to achieve the result
+        // for (let key in snapshotValues) {
+        //     leads.push(snapshotValues[key])
+        // }
+
+        render(leads)
+    }
+
+
+})
+
 deleteBtn.addEventListener("dblclick", function() {
-    localStorage.clear()
-    myLeads = []
-    render(myLeads)
+    remove(referenceInDB)
+    ulEl.innerHTML = ""
 })
 
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
+    push(referenceInDB, inputEl.value)
     inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    render(myLeads)
 })
